70edd766e2617a9d25263e6fcd4e46d12be4ff66
Akyl1988 <kakimov.akyl1988@gmail.com>
2025-09-12 10:36:08 +0500

    Update app/main.py with new FastAPI code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
-
 from fastapi import FastAPI
 
 def create_app() -> FastAPI:
     app = FastAPI(title="SmartSell3 API")
-    # TODO: подключи роуты, модели, middleware
+    # TODO: подключить роуты и middleware
     return app
 
 app = create_app()
